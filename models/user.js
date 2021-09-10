63ccf1daefba6c384130643758e6faf2bde52ffa
FERNANDONB\ichemming <fernandoscharnick33@gmail.com>
2021-09-10 14:39:17 +0200

    Added new user model file

---
+++ models/user.js
@@ -0,0 +1,10 @@
+class User {
+  constructor(id, userId, fullName, surname) {
+    this.id = id;
+    this.userId = userId;
+    this.fullName = fullName;
+    this.surname = surname;
+  }
+}
+
+export default User;

945cde46500de12f89060154e0dbdc69e4f46ff9
FERNANDONB\ichemming <fernandoscharnick33@gmail.com>
2021-09-10 16:38:55 +0200

    Modified the user Modal: added race

--- models/user.js
+++ models/user.js
@@ -1,9 +1,10 @@
 class User {
-  constructor(id, userId, fullName, surname) {
+  constructor(id, userId, fullName, surname, race) {
     this.id = id;
     this.userId = userId;
     this.fullName = fullName;
     this.surname = surname;
+    this.race = race;
   }
 }
 
